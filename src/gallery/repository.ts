8c767f7af8ee1d0d7ccf35ef30ba10c42a173e9e
donikaiy <doni94638@gmail.com>
2025-04-15 16:14:48 +0300

    feat: feat: add preferredGalleryEntryId to hotels for selecting main page image

--- src/gallery/repository.ts
+++ src/gallery/repository.ts
@@ -1,5 +1,6 @@
 import {Gallery, GalleryDB} from "./gallery";
 import {connection} from "../repository";
+import {placeholderIds} from "../utils/database";
 
 const getGalleryImagesByGalleryId = async (galleryId: number): Promise<Gallery[]> => {
     const [results] = await connection.execute<GalleryDB[]>('SELECT * FROM gallery WHERE gallery_id = ?', [galleryId]);
@@ -15,4 +16,18 @@ const getGalleryImagesByGalleryId = async (galleryId: number): Promise<Gallery[]
     })
 }
 
-export default {getGalleryImagesByGalleryId}
+const getAllGalleryImagesByGalleryEntries = async (galleryEntries: number[]): Promise<Gallery[]> => {
+    const [results] = await connection.execute<GalleryDB[]>(`SELECT * FROM gallery WHERE gallery_entry IN (${placeholderIds(galleryEntries)})`, galleryEntries)
+
+    return results.map(galleryDB => {
+        const gallery: Gallery = {
+            galleryEntry: galleryDB.gallery_entry,
+            galleryId: galleryDB.gallery_id,
+            imageUrl: galleryDB.image_url
+        }
+
+        return gallery;
+    })
+}
+
+export default {getGalleryImagesByGalleryId, getAllGalleryImagesByGalleryEntries}
--- src/gallery/service.ts
+++ src/gallery/service.ts
@@ -3,3 +3,7 @@ import galleryRepository from '../gallery/repository'
 export const getGalleryImagesByGalleryId = async (galleryId: number) => {
     return galleryRepository.getGalleryImagesByGalleryId(galleryId)
 }
+
+export const getAllGalleryImagesByGalleryEntries = async (galleryEntries: number[]) => {
+    return galleryRepository.getAllGalleryImagesByGalleryEntries(galleryEntries)
+}
