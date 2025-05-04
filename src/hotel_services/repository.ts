7d42831c5422d864606659ad1e0e470741f9ebe6
Donika Yordanova <115694566+donikaiy@users.noreply.github.com>
2025-05-04 21:58:18 +0300

    refactor: establish direct one-to-many relationship between hotels and services (#11)

--- src/hotel_services/repository.ts
+++ src/hotel_services/repository.ts
@@ -2,11 +2,12 @@ import {connection} from "../repository";
 import {HotelService, HotelServiceDB} from "./domain";
 
 const getAllServicesByHotelId = async (hotelId: number): Promise<HotelService[]> => {
-    const [results] = await connection.execute<HotelServiceDB[]>(`SELECT * FROM hotels_services WHERE hotel_id = ?`, [hotelId]);
+    const [results] = await connection.execute<HotelServiceDB[]>(`SELECT * FROM hotel_services WHERE hotel_id = ?`, [hotelId]);
     return results.map(hotelServiceDB => {
         const hotelService: HotelService = {
+            hotelServiceId: hotelServiceDB.hotel_service_id,
             hotelId: hotelServiceDB.hotel_id,
-            serviceId: hotelServiceDB.service_id,
+            text: hotelServiceDB.text,
         }
 
         return hotelService
--- src/hotel_services/service.ts
+++ src/hotel_services/service.ts
@@ -1,10 +1,5 @@
 import hotelServiceRepository from "../hotel_services/repository";
-import {getServicesByIds} from "../services/service";
 
 export const getAllServicesByHotelId = async (hotelId: number) => {
-    const hotelServices = await hotelServiceRepository.getAllServicesByHotelId(hotelId)
-
-    const serviceIds: number[] = hotelServices.map(service => service.serviceId);
-
-    return getServicesByIds(serviceIds)
+    return hotelServiceRepository.getAllServicesByHotelId(hotelId)
 }
