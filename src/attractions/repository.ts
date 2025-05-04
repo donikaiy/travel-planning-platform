a0780c6e3911179084a8250fa9e16101a1e489b2
donikaiy <doni94638@gmail.com>
2025-05-04 22:03:05 +0300

    refactor: update getAttractionById to return a single Attraction object and handle not found error

--- src/attractions/repository.ts
+++ src/attractions/repository.ts
@@ -22,24 +22,27 @@ const getAllAttractions = async (): Promise<Attraction[]> => {
     })
 }
 
-const getAttractionById = async (attractionId: number): Promise<Attraction[]> => {
+const getAttractionById = async (attractionId: number): Promise<Attraction> => {
     const [result] = await connection.execute<AttractionDB[]>('SELECT * FROM attractions WHERE attraction_id = ?', [attractionId])
-    return result.map(attractionDB => {
-        const attraction: Attraction = {
-            attractionId: attractionDB.attraction_id,
-            cityId: attractionDB.city_id,
-            name: attractionDB.name,
-            location: attractionDB.location,
-            imageUrl: attractionDB.image_url,
-            description: attractionDB.description,
-            openingHours: attractionDB.opening_hours,
-            bestTimeToVisit: attractionDB.best_time_to_visit,
-            ticketsWebsite: attractionDB.tickets_website,
-            additionalInformation: attractionDB.additional_information,
-        }
 
-        return attraction
-    })
+    if (result.length === 0) {
+        throw new Error('Attraction not found')
+    }
+
+    const attractionDB = result[0]
+
+    return {
+        attractionId: attractionDB.attraction_id,
+        cityId: attractionDB.city_id,
+        name: attractionDB.name,
+        location: attractionDB.location,
+        imageUrl: attractionDB.image_url,
+        description: attractionDB.description,
+        openingHours: attractionDB.opening_hours,
+        bestTimeToVisit: attractionDB.best_time_to_visit,
+        ticketsWebsite: attractionDB.tickets_website,
+        additionalInformation: attractionDB.additional_information,
+    }
 }
 
 const getAttractionsByIds = async (ids: number[]): Promise<Attraction[]> => {
