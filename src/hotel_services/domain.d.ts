9b7abc2fd2923d573b7f0e850d5c97a16ebff189
donikaiy <doni94638@gmail.com>
2025-05-04 21:30:54 +0300

    refactor: establish direct one-to-many relationship between hotels and services

--- src/hotel_services/domain.d.ts
+++ src/hotel_services/domain.d.ts
@@ -1,11 +1,13 @@
 import {RowDataPacket} from "mysql2";
 
 export type HotelService = {
+    hotelServiceId: number,
     hotelId: number,
-    serviceId: number,
+    text: string
 }
 
 export type HotelServiceDB = RowDataPacket & {
+    hotel_service_id: number,
     hotel_id: number,
-    service_id: number,
+    text: string
 }
--- src/services/service.ts
+++
@@ -1,5 +0,0 @@
-import repository from "./repository";
-
-export const getServicesByIds = async (ids: number[]) => {
-    return repository.getServicesByIds(ids)
-}
