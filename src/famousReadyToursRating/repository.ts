50732a294125ebc5b0854cd1c3d12c9c85eaf87b
Donika Yordanova <115694566+donikaiy@users.noreply.github.com>
2025-05-04 22:12:45 +0300

    feat: add ratings to getAllReadyTours response (#12)

--- src/famousReadyToursRating/repository.ts
+++ src/famousReadyToursRating/repository.ts
@@ -1,17 +1,27 @@
 import {connection} from "../repository";
 import {FamousReadyTourRating, FamousReadyTourRatingDB} from "./domain";
+import {placeholderIds} from "../utils/database";
 
-const getAllReadyToursRatingByReadyTourId = async (readyTourId: number): Promise<FamousReadyTourRating[]> => {
-    const [results] = await connection.execute<FamousReadyTourRatingDB[]>(`SELECT * FROM famous_ready_tours_rating WHERE ready_tour_id = ?`, [readyTourId]);
-    return results.map(ratingDB => {
+const getAllReadyToursRatingsByReadyTourIdsMap = async (ids: number[]): Promise<Map<number, FamousReadyTourRating[]>> => {
+    const [results] = await connection.execute<FamousReadyTourRatingDB[]>(`SELECT * FROM famous_ready_tours_rating WHERE ready_tour_id IN (${placeholderIds(ids)})`, ids);
+
+    const ratingsMap = new Map<number, FamousReadyTourRating[]>();
+
+    results.forEach(ratingDB => {
         const rating: FamousReadyTourRating = {
             ratingId: ratingDB.rating_id,
             readyTourId: ratingDB.ready_tour_id,
             rating: ratingDB.rating,
         }
 
-        return rating
+        if (ratingsMap.has(rating.readyTourId)) {
+            ratingsMap.get(rating.readyTourId)!.push(rating)
+        } else {
+            ratingsMap.set(rating.readyTourId, [rating])
+        }
     })
+
+    return ratingsMap
 }
 
-export default {getAllReadyToursRatingByReadyTourId}
+export default {getAllReadyToursRatingsByReadyTourIdsMap}
--- src/readyTours/service.ts
+++ src/readyTours/service.ts
@@ -1,17 +1,27 @@
 import readyTourRepository from "../readyTours/repository";
 import {getAllTourProgramsByReadyTourId} from "../tourPrograms/service";
-import {getReadyToursRatingByReadyTourId} from "../famousReadyToursRating/service";
+import {getRatingsByReadyToursIdsMap} from "../famousReadyToursRating/service";
 
 export const getAllReadyTours = async () => {
-    return readyTourRepository.getAllReadyTours()
+    const readyTours = await readyTourRepository.getAllReadyTours()
+
+    const readyToursIds = readyTours.map(readyTourId => readyTourId.readyTourId)
+
+    const ratings = await getRatingsByReadyToursIdsMap(readyToursIds)
+
+    return readyTours.map(readyTour => {
+        return {
+            ...readyTour,
+            rating: ratings.get(readyTour.readyTourId)
+        }
+    })
 }
 
 export const getReadyTourById = async (readyTourId: number) => {
-    const [readyTour, tourProgram, rating] = await Promise.all([
+    const [readyTour, tourProgram] = await Promise.all([
         readyTourRepository.getReadyTourById(readyTourId),
         getAllTourProgramsByReadyTourId(readyTourId),
-        getReadyToursRatingByReadyTourId(readyTourId)
     ])
 
-    return {readyTour: readyTour, rating: rating, tourProgram: tourProgram}
+    return {readyTour: readyTour, tourProgram: tourProgram}
 }
