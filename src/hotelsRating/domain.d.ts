d5c52100d3c7cb591a7625fc9ac938cfacba48fe
donikaiy <doni94638@gmail.com>
2025-05-05 17:23:25 +0300

    refactor: rename FamousHotelsRating to hotelsRating

--- src/hotelsRating/domain.d.ts
+++ src/hotelsRating/domain.d.ts
@@ -1,12 +1,12 @@
 import {RowDataPacket} from "mysql2";
 
-export type FamousHotelRating = {
+export type hotelRating = {
     ratingId: number,
     hotelId: number,
     rating: number,
 }
 
-export type FamousHotelRatingDb = RowDataPacket & {
+export type hotelRatingDb = RowDataPacket & {
     rating_id: number,
     hotel_id: number,
     rating: number,
--- src/hotelsRating/repository.ts
+++ src/hotelsRating/repository.ts
@@ -1,14 +1,14 @@
-import {FamousHotelRating, FamousHotelRatingDb} from "./domain";
+import {hotelRating, hotelRatingDb} from "./domain";
 import {connection} from "../repository";
 import {placeholderIds} from "../utils/database";
 
-const getAllHotelRatingsByHotelIdMap = async (ids: number[]): Promise<Map<number, FamousHotelRating[]>> => {
-    const [results] = await connection.execute<FamousHotelRatingDb[]>(`SELECT * FROM famous_hotels_rating WHERE hotel_id IN (${placeholderIds(ids)})`, ids);
+const getAllHotelRatingsByHotelIdMap = async (ids: number[]): Promise<Map<number, hotelRating[]>> => {
+    const [results] = await connection.execute<hotelRatingDb[]>(`SELECT * FROM hotels_rating WHERE hotel_id IN (${placeholderIds(ids)})`, ids);
 
-    const ratingsMap = new Map<number, FamousHotelRating[]>();
+    const ratingsMap = new Map<number, hotelRating[]>();
 
     results.forEach(ratingDb => {
-        const rating: FamousHotelRating = {
+        const rating: hotelRating = {
             ratingId: ratingDb.rating_id,
             hotelId: ratingDb.hotel_id,
             rating: ratingDb.rating,
