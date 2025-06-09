71ce307d815caedd4a44d172064d42f0549b1310
donikaiy <doni94638@gmail.com>
2025-06-09 15:18:24 +0300

    feat: implement create, update and delete functionality for flights

--- backend/src/flights/handler.ts
+++ backend/src/flights/handler.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import type {Request, Response} from "express";
-import {getRoundTrip} from "./service.ts";
+import {createFlight, deleteFlightById, getAllFlights, getRoundTrip, updateFlightById} from "./service.ts";
+import {ALREADY_EXISTS} from "../utils/responseMessages.ts";
 
 const router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const flights = await getAllFlights()
+
+        res.json(flights)
+    } catch (err: any) {
+        res.status(500).json({error: err.message})
+    }
+})
 
 router.get('/round-trip', async (req: Request, res: Response) => {
     try {
@@ -14,6 +25,38 @@ router.get('/round-trip', async (req: Request, res: Response) => {
         });
 
         res.json(roundTrip)
+    } catch (err: any) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const newFlight = await createFlight(req.body.originCityId, req.body.destinationCityId, req.body.departAt, req.body.arriveAt, req.body.numberOfStops, req.body.price, req.body.imageUrl, req.body.airline)
+        res.status(201).json(newFlight)
+    } catch (err: any) {
+        if (err.message === ALREADY_EXISTS) {
+            res.status(409).json({error: err.message});
+            return
+        }
+
+        res.status(500).json({error: err.message})
+    }
+})
+
+router.delete('/', async (req: Request, res: Response) => {
+    try {
+        await deleteFlightById(Number(req.body.flightId))
+        res.status(200)
+    } catch (err: any) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+router.put('/', async (req: Request, res: Response) => {
+    try {
+        await updateFlightById(req.body.flightId, req.body.originCityId, req.body.destinationCityId, req.body.departAt, req.body.arriveAt, req.body.numberOfStops, req.body.price, req.body.imageUrl, req.body.airline)
+        res.status(200)
     } catch (err: any) {
         res.status(500).json({error: err.message})
     }
--- backend/src/flights/repository.ts
+++ backend/src/flights/repository.ts
@@ -1,6 +1,8 @@
 import type {Flight, FlightDb} from "./domain.d.ts";
 import {connection} from "../repository.ts";
 import type {CityDb} from "../cities/domain.d.ts";
+import type {QueryResult, ResultSetHeader} from "mysql2";
+import type { Exist } from "../types/exist.d.ts";
 
 export type Filters = {
     departureCityId?: number,
@@ -46,7 +48,7 @@ const getAllFlights = async (filters: Filters = {}): Promise<Flight[]> => {
     })
 }
 
-const getUniqueCityIdsFromFlights = async () => {
+const getUniqueCityIdsFromFlights = async (): Promise<number[]> => {
     const [results] = await connection.query<CityDb[]>(
         'SELECT origin_city_id AS city_id FROM flights UNION SELECT destination_city_id AS city_id FROM flights'
     );
@@ -54,6 +56,39 @@ const getUniqueCityIdsFromFlights = async () => {
     return results.map(result => result.city_id);
 };
 
+const checkFlightExists = async (originCityId: number, destinationCityId: number, departAt: string, arriveAt: string, airline: string): Promise<boolean> => {
+    const [result] = await connection.execute<Exist[]>('SELECT EXISTS(SELECT * FROM flights WHERE origin_city_id = ? AND destination_city_id = ? AND depart_at = ? AND arrive_at = ? AND airline = ?) AS exist', [originCityId, destinationCityId, departAt, arriveAt, airline])
 
-export default {getAllFlights, getUniqueCityIdsFromFlights}
+    return result[0]?.exist === 1
+}
+
+const createFlight = async (originCityId: number, destinationCityId: number, departAt: string, arriveAt: string, numberOfStops: number, price: number, imageUrl: string, airline: string): Promise<Flight> => {
+    const [result] = await connection.execute<ResultSetHeader>('INSERT INTO flights (origin_city_id, destination_city_id, depart_at, arrive_at, number_of_stops, price, image_url, airline) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [originCityId, destinationCityId, departAt, arriveAt, numberOfStops, price, imageUrl, airline]);
+
+    return {
+        flightId: result.insertId,
+        originCityId,
+        destinationCityId,
+        departAt,
+        arriveAt,
+        numberOfStops,
+        price,
+        imageUrl,
+        airline,
+    }
+}
+
+const deleteFlightById = async (flightId: number): Promise<QueryResult> => {
+    const [result] = await connection.execute('DELETE FROM flights WHERE flight_id = ?', [flightId]);
+
+    return result
+}
+
+const updateFlightById = async (flightId: number, originCityId: number, destinationCityId: number, departAt: string, arriveAt: string, numberOfStops: number, price: number, imageUrl: string, airline: string): Promise<QueryResult> => {
+    const [result] = await connection.execute('UPDATE flights SET origin_city_id = ?, destination_city_id = ?, depart_at = ?, arrive_at = ?, number_of_stops = ?, price = ?, image_url = ?, airline = ? WHERE flight_id = ?', [originCityId, destinationCityId, departAt, arriveAt, numberOfStops, price, imageUrl, airline, flightId]);
+
+    return result
+}
+
+export default {getAllFlights, getUniqueCityIdsFromFlights, checkFlightExists, createFlight, deleteFlightById, updateFlightById}
 
