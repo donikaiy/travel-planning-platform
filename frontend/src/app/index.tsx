080f6ee1837b0e63193ad341def54e1b198e65b0
Donika Yordanova <115694566+donikaiy@users.noreply.github.com>
2025-06-10 22:17:47 +0300

    chore: set up TanStack React Query and configure Axios instance with base URL from environment (#46)

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,13 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./routes/home.tsx"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const Index = () => {
+    const queryClient = new QueryClient()
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
 
     )
 }
